0dbcf0ad38cd1d241aee224f21c73305887a60cc
zimb <1050519810@qq.com>
2024-12-21 02:00:42 +0800

    first commit

---
+++ mybot.py
@@ -0,0 +1,35 @@
+import asyncio
+import os
+
+import botpy
+from botpy.message import Message
+from botpy.ext.command_util import Commands
+from botpy import logging, BotAPI
+from botpy.ext.cog_yaml import read
+
+_log = logging.get_logger()
+config = read(os.path.join(os.path.dirname(__file__), "config.yml"))
+
+class MyClient(botpy.Client):
+    async def on_ready(self):
+        _log.info(f"robot 「{self.robot.name}」 on_ready!")
+
+    async def on_at_message_create(self, message: Message):
+        _log.info(message.author.avatar)
+        if "sleep" in message.content:
+            await asyncio.sleep(10)
+        _log.info(message.author.username)
+        await message.reply(content=f"机器人{self.robot.name}收到你的@消息了: {message.content}")
+
+
+@Commands("test")
+async def test(api: BotAPI, message: Message, params=None):
+    await message.reply(content=params)
+    await api.post_message(channel_id=message.channel_id, content=params, msg_id=message.id)
+    return True
+
+
+if __name__ == "__main__":
+    intents = botpy.Intents(public_guild_messages=True)
+    client = MyClient(intents=intents)
+    client.run(appid=config['appid'], secret=config['secret'])
