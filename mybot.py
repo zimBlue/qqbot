be34e89fab0fca403e00c16dd7d802cfcde6afda
zimb <1050519810@qq.com>
2024-12-21 14:35:39 +0800

    first commit

--- mybot.py
+++ mybot.py
@@ -14,13 +14,6 @@ class MyClient(botpy.Client):
     async def on_ready(self):
         _log.info(f"robot 「{self.robot.name}」 on_ready!")
 
-    async def on_at_message_create(self, message: Message):
-        _log.info(message.author.avatar)
-        if "sleep" in message.content:
-            await asyncio.sleep(10)
-        _log.info(message.author.username)
-        await message.reply(content=f"机器人{self.robot.name}收到你的@消息了: {message.content}")
-
     async def on_group_at_message_create(self, message: GroupMessage):
         messageResult = await message._api.post_group_message(
             group_openid=message.group_openid,
@@ -28,6 +21,13 @@ class MyClient(botpy.Client):
               msg_id=message.id,
               content=f"收到了消息：{message.content}")
         _log.info(messageResult)
+
+    async def on_at_message_create(self, message: Message):
+        _log.info(message.author.avatar)
+        if "sleep" in message.content:
+            await asyncio.sleep(10)
+        _log.info(message.author.username)
+        await message.reply(content=f"机器人{self.robot.name}收到你的@消息了: {message.content}")
 
 
 @Commands("test")
